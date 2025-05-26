1a3df884ed24e980e1080db1b384b64f2d17da9a
RiinaKuu <riina.kuukso@funidata.fi>
2025-05-26 16:17:27 +0300

    DS-504: Add Notification styles

---
+++ core/src/components/notification/notification.stories.js
@@ -0,0 +1,35 @@
+export default {
+  title: "Components/Notification",
+};
+
+export const Danger = () => `
+  <article class="fudis-notification fudis-notification__danger">
+    <div class="fudis-notification__content">
+      <p class="fudis-body-text fudis-body-text__md-regular">Danger</p>
+    </div>
+  </article>
+`;
+
+export const Info = () => `
+  <article class="fudis-notification fudis-notification__info">
+    <div class="fudis-notification__content">
+      <p class="fudis-body-text fudis-body-text__md-regular">Info</p>
+    </div>
+  </article>
+`;
+
+export const Success = () => `
+  <article class="fudis-notification fudis-notification__success">
+    <div class="fudis-notification__content">
+      <p class="fudis-body-text fudis-body-text__md-regular">Success</p>
+    </div>
+  </article>
+`;
+
+export const Warning = () => `
+  <article class="fudis-notification fudis-notification__warning">
+    <div class="fudis-notification__content">
+      <p class="fudis-body-text fudis-body-text__md-regular">Warning</p>
+    </div>
+  </article>
+`;
