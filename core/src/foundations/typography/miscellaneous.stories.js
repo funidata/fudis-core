1b04988747e89ccd79efb776a8f8eaf31c61b29b
RiinaKuu <riina.kuukso@funidata.fi>
2025-05-28 17:41:10 +0300

    DS-504: Remove unnecessary decorators, add indentation for better readability, rename header to heading stories

--- core/src/foundations/typography/miscellaneous.stories.js
+++ core/src/foundations/typography/miscellaneous.stories.js
@@ -1,19 +1,15 @@
 export default {
     title: "Components/Typography/Miscellaneous",
-    decorators: [
-        (storyFn) =>
-            `<div>${storyFn()}</div>`,
-    ],
 };
 
 export const EmptyState = () => `
-<p class="fudis-text-emptystate">
-Fudis empty state text
-</p>
+  <p class="fudis-text-emptystate">
+    Fudis empty state text
+  </p>
 `;
 
 export const TableCaption = () => `
-<p class="fudis-table-caption">
-Fudis table caption
-</p>
+  <p class="fudis-table-caption">
+    Fudis table caption
+  </p>
 `;
