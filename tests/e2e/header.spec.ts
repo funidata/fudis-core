836a4a61045ec8c9d4fb654363390580d7e51d1e
marisanity <marion.karlsson@gmail.com>
2025-05-30 11:06:54 +0300

    DS-487 fix typo

--- tests/e2e/header.spec.ts
+++ tests/e2e/header.spec.ts
@@ -1,6 +1,6 @@
 import {expect, test} from "@playwright/test";
 
 test('visual regression', async ({ page }) => {
-  await page.goto('iframe.html?globals=&id=components-typography-header--test&viewMode=story');
+  await page.goto('/iframe.html?globals=&id=components-typography-header--test&viewMode=story');
   await expect(page).toHaveScreenshot("headers.png", { fullPage: true });
 })
