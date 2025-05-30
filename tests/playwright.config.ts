29527ca9ecbe5d0c6489fe9ceaf8dfb98f112195
marisanity <marion.karlsson@gmail.com>
2025-05-30 20:45:10 +0300

    DS-487 fixes from review

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -4,14 +4,14 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./e2e",
+  testDir: "./visual-regression",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
+  /* Opt in parallel tests on CI. */
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
@@ -46,5 +46,9 @@ export default defineConfig({
       name: "Mobile Safari",
       use: { ...devices["iPhone 12"] },
     },
+    {
+      name: "Mobile Chrome Big Landscape",
+      use: {...devices["Pixel 7 landscape"]},
+    }
   ],
 });
