db26d608eccacef7f393c483a13eb20cc2824de8
marisanity <marion.karlsson@gmail.com>
2025-06-03 14:05:43 +0300

    align icons in story

--- core/src/components/icon/icon.stories.js
+++ core/src/components/icon/icon.stories.js
@@ -2,15 +2,32 @@ export default {
   title: "Components/Icon",
   decorators: [
     (storyFn) =>
-      `<div style="display: flex; gap: 12px; flex-wrap: wrap; align-items: flex-start;">${storyFn()}</div>`,
+      `<div style="display: grid; gap: 12px; grid-template-columns: 30px 30px 30px; align-items: center;">${storyFn()}</div>`,
   ],
 };
 
 export const AllIcons = () => `
+  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color--primary fudis-icon__achievement"></span>
+  <span class="fudis-icon fudis-icon__size--md fudis-icon__color--primary fudis-icon__achievement"></span>
   <span class="fudis-icon fudis-icon__size--lg fudis-icon__color--primary fudis-icon__achievement"></span>
+
+  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color--primary fudis-icon__alert"></span>
+  <span class="fudis-icon fudis-icon__size--md fudis-icon__color--primary fudis-icon__alert"></span>
   <span class="fudis-icon fudis-icon__size--lg fudis-icon__color--primary fudis-icon__alert"></span>
+
+  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color--primary fudis-icon__archive"></span>
+  <span class="fudis-icon fudis-icon__size--md fudis-icon__color--primary fudis-icon__archive"></span>
   <span class="fudis-icon fudis-icon__size--lg fudis-icon__color--primary fudis-icon__archive"></span>
+
+  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color--primary fudis-icon__arrow-big"></span>
+  <span class="fudis-icon fudis-icon__size--md fudis-icon__color--primary fudis-icon__arrow-big"></span>
   <span class="fudis-icon fudis-icon__size--lg fudis-icon__color--primary fudis-icon__arrow-big"></span>
+
+  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color--primary fudis-icon__arrow-dashed"></span>
+  <span class="fudis-icon fudis-icon__size--md fudis-icon__color--primary fudis-icon__arrow-dashed"></span>
   <span class="fudis-icon fudis-icon__size--lg fudis-icon__color--primary fudis-icon__arrow-dashed"></span>
+
+  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color--primary fudis-icon__arrow-solid"></span>
+  <span class="fudis-icon fudis-icon__size--md fudis-icon__color--primary fudis-icon__arrow-solid"></span>
   <span class="fudis-icon fudis-icon__size--lg fudis-icon__color--primary fudis-icon__arrow-solid"></span>
 `;
