e04cbca6df60ee409030b4d670e8243765c1ea36
marisanity <marion.karlsson@gmail.com>
2025-06-03 15:11:40 +0300

    fix styles

--- core/src/components/icon/icon.stories.js
+++ core/src/components/icon/icon.stories.js
@@ -7,27 +7,27 @@ export default {
 };
 
 export const AllIcons = () => `
-  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color--primary fudis-icon__achievement"></span>
-  <span class="fudis-icon fudis-icon__size--md fudis-icon__color--primary fudis-icon__achievement"></span>
-  <span class="fudis-icon fudis-icon__size--lg fudis-icon__color--primary fudis-icon__achievement"></span>
+  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color__primary fudis-icon__achievement"></span>
+  <span class="fudis-icon fudis-icon__size--md fudis-icon__color__primary fudis-icon__achievement"></span>
+  <span class="fudis-icon fudis-icon__size--lg fudis-icon__color__primary fudis-icon__achievement"></span>
 
-  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color--primary fudis-icon__alert"></span>
-  <span class="fudis-icon fudis-icon__size--md fudis-icon__color--primary fudis-icon__alert"></span>
-  <span class="fudis-icon fudis-icon__size--lg fudis-icon__color--primary fudis-icon__alert"></span>
+  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color__primary fudis-icon__alert"></span>
+  <span class="fudis-icon fudis-icon__size--md fudis-icon__color__primary fudis-icon__alert"></span>
+  <span class="fudis-icon fudis-icon__size--lg fudis-icon__color__primary fudis-icon__alert"></span>
 
-  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color--primary fudis-icon__archive"></span>
-  <span class="fudis-icon fudis-icon__size--md fudis-icon__color--primary fudis-icon__archive"></span>
-  <span class="fudis-icon fudis-icon__size--lg fudis-icon__color--primary fudis-icon__archive"></span>
+  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color__primary fudis-icon__archive"></span>
+  <span class="fudis-icon fudis-icon__size--md fudis-icon__color__primary fudis-icon__archive"></span>
+  <span class="fudis-icon fudis-icon__size--lg fudis-icon__color__primary fudis-icon__archive"></span>
 
-  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color--primary fudis-icon__arrow-big"></span>
-  <span class="fudis-icon fudis-icon__size--md fudis-icon__color--primary fudis-icon__arrow-big"></span>
-  <span class="fudis-icon fudis-icon__size--lg fudis-icon__color--primary fudis-icon__arrow-big"></span>
+  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color__primary fudis-icon__arrow-big"></span>
+  <span class="fudis-icon fudis-icon__size--md fudis-icon__color__primary fudis-icon__arrow-big"></span>
+  <span class="fudis-icon fudis-icon__size--lg fudis-icon__color__primary fudis-icon__arrow-big"></span>
 
-  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color--primary fudis-icon__arrow-dashed"></span>
-  <span class="fudis-icon fudis-icon__size--md fudis-icon__color--primary fudis-icon__arrow-dashed"></span>
-  <span class="fudis-icon fudis-icon__size--lg fudis-icon__color--primary fudis-icon__arrow-dashed"></span>
+  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color__primary fudis-icon__arrow-dashed"></span>
+  <span class="fudis-icon fudis-icon__size--md fudis-icon__color__primary fudis-icon__arrow-dashed"></span>
+  <span class="fudis-icon fudis-icon__size--lg fudis-icon__color__primary fudis-icon__arrow-dashed"></span>
 
-  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color--primary fudis-icon__arrow-solid"></span>
-  <span class="fudis-icon fudis-icon__size--md fudis-icon__color--primary fudis-icon__arrow-solid"></span>
-  <span class="fudis-icon fudis-icon__size--lg fudis-icon__color--primary fudis-icon__arrow-solid"></span>
+  <span class="fudis-icon fudis-icon__size--sm fudis-icon__color__primary fudis-icon__arrow-solid"></span>
+  <span class="fudis-icon fudis-icon__size--md fudis-icon__color__primary fudis-icon__arrow-solid"></span>
+  <span class="fudis-icon fudis-icon__size--lg fudis-icon__color__primary fudis-icon__arrow-solid"></span>
 `;
