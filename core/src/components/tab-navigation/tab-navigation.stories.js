61c1cfc492a8d3dffc85627a162274d7848a0b88
RiinaKuu <riina.kuukso@funidata.fi>
2025-06-04 15:16:23 +0300

    DS-504: Add accessibility additions to TabNavigation

--- core/src/components/tab-navigation/tab-navigation.stories.js
+++ core/src/components/tab-navigation/tab-navigation.stories.js
@@ -9,11 +9,16 @@ export const Primary = () => `
   <div class="fudis-tab-navigation-bar">
     <div class="fudis-tab-navigation-bar__wrapper fudis-tab-navigation-bar__wrapper--primary">
       <div class="fudis-tab-navigation-bar__wrapper__tab-container fudis-tab-navigation-bar__wrapper__tab-container--primary">
-        <button role="tab" class="fudis-tab-navigation-tab fudis-tab-navigation-tab--primary fudis-tab-navigation-tab--primary--active">Tab 1</button>
-        <button role="tab" class="fudis-tab-navigation-tab fudis-tab-navigation-tab--primary">Tab 2</button>
-        <button role="tab" class="fudis-tab-navigation-tab fudis-tab-navigation-tab--primary">Tab 3</button>
+        <button role="tab" aria-controls="fudis-panel-1" id="fudis-tab-1" class="fudis-tab-navigation-tab fudis-tab-navigation-tab--primary fudis-tab-navigation-tab--primary--active" aria-selected="true" tabindex="0">Tab 1</button>
+        <button role="tab" class="fudis-tab-navigation-tab fudis-tab-navigation-tab--primary" tabindex="-1">Tab 2</button>
+        <button role="tab" class="fudis-tab-navigation-tab fudis-tab-navigation-tab--primary" tabindex="-1">Tab 3</button>
       </div>
     </div>
+  </div>
+  <div role="tabpanel" id="fudis-panel-1" aria-labelledby="fudis-tab-1" style="margin-top: 1rem;">
+    <p class="fudis-body-text fudis-body-text__left fudis-body-text__md-regular fudis-body-text__default">
+      Tab content
+    </p>
   </div>
 `;
 
@@ -21,10 +26,15 @@ export const Secondary = () => `
   <div class="fudis-tab-navigation-bar">
     <div class="fudis-tab-navigation-bar__wrapper fudis-tab-navigation-bar__wrapper--secondary">
       <div class="fudis-tab-navigation-bar__wrapper__tab-container fudis-tab-navigation-bar__wrapper__tab-container--secondary">
-        <button role="tab" class="fudis-tab-navigation-tab fudis-tab-navigation-tab--secondary fudis-tab-navigation-tab--secondary--active">Tab 1</button>
-        <button role="tab" class="fudis-tab-navigation-tab fudis-tab-navigation-tab--secondary">Tab 2</button>
-        <button role="tab" class="fudis-tab-navigation-tab fudis-tab-navigation-tab--secondary">Tab 3</button>
+        <button role="tab" aria-controls="fudis-panel-1" id="fudis-tab-1" class="fudis-tab-navigation-tab fudis-tab-navigation-tab--secondary fudis-tab-navigation-tab--secondary--active" aria-selected="true" tabindex="0">Tab 1</button>
+        <button role="tab" class="fudis-tab-navigation-tab fudis-tab-navigation-tab--secondary" tabindex="-1">Tab 2</button>
+        <button role="tab" class="fudis-tab-navigation-tab fudis-tab-navigation-tab--secondary" tabindex="-1">Tab 3</button>
       </div>
     </div>
+  </div>
+  <div role="tabpanel" id="fudis-panel-1" aria-labelledby="fudis-tab-1" style="margin-top: 1rem;">
+    <p class="fudis-body-text fudis-body-text__left fudis-body-text__md-regular fudis-body-text__default">
+      Tab content
+    </p>
   </div>
 `;
