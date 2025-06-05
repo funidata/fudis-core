a5269a5d997a998b7cbbde3ff4faf3d0559d697f
marisanity <marion.karlsson@gmail.com>
2025-06-05 16:39:13 +0300

    DS-502 ignore all sidebar items that container pw, update tests and story names

--- tests/visual-regression/body-text.spec.ts
+++ tests/visual-regression/body-text.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@playwright/test";
 
 test("visual regression for Body Text", async ({ page }) => {
   await page.goto(
-    "/iframe.html?globals=&id=components-typography-bodytext--test&viewMode=story",
+    "/iframe.html?globals=&id=components-typography-bodytext--pw-all&viewMode=story",
   );
   await expect(page).toHaveScreenshot("body-texts.png", { fullPage: true });
 });
--- tests/visual-regression/miscellaneous.spec.ts
+++ tests/visual-regression/miscellaneous.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@playwright/test";
 
 test("visual regression for Miscellaneous", async ({ page }) => {
   await page.goto(
-    "/iframe.html?globals=&id=components-typography-miscellaneous--test&viewMode=story",
+    "/iframe.html?globals=&id=components-typography-miscellaneous--pw-all&viewMode=story",
   );
   await expect(page).toHaveScreenshot("miscellaneous.png", { fullPage: true });
 });
