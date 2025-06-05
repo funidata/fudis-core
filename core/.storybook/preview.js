524f006c774fd653470acc6849ad124f88979516
marisanity <marion.karlsson@gmail.com>
2025-06-05 14:19:25 +0300

    DS-502 update preview and rename links in Guide

--- core/.storybook/preview.js
+++ core/.storybook/preview.js
@@ -16,7 +16,7 @@ const preview = {
             "Introduction",
             ["Introduction", "How to start using Fudis Core"],
             "Development",
-            ["Getting Started", "Tooling", ["Releases"]],
+            ["Getting Started", "Tooling", ["Git and GitHub Conventions", "Releases"], "Practises", ["Ways of Working", "Project Structure", "Naming Conventions", "Testing practises"]],
           ],
           "Foundations",
           [
