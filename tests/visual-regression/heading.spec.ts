800d9bba43ab8374364fcffa42a4154b17accf7b
marisanity <marion.karlsson@gmail.com>
2025-06-06 10:28:18 +0300

    DS-502 cleanup after rebase

--- tests/visual-regression/heading.spec.ts
+++ tests/visual-regression/heading.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@playwright/test";
 
 test("visual regression for Heading", async ({ page }) => {
   await page.goto(
-    "/iframe.html?globals=&id=components-typography-heading--test&viewMode=story",
+    "/iframe.html?globals=&id=components-typography-heading--pw-all&viewMode=story",
   );
   await expect(page).toHaveScreenshot("headings.png", { fullPage: true });
 });
