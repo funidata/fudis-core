34b0c291701055784b834007945380584a51d483
marisanity <marion.karlsson@gmail.com>
2025-06-06 11:56:53 +0300

    DS-502 fix link url in spec file

--- tests/visual-regression/link.spec.ts
+++ tests/visual-regression/link.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@playwright/test";
 
 test("visual regression for Link", async ({ page }) => {
   await page.goto(
-    "/iframe.html?globals=&id=components-typography-link-wip--pw-all&viewMode=story",
+    "/iframe.html?globals=&id=components-link-wip--pw-all&viewMode=story",
   );
   await expect(page).toHaveScreenshot("links.png", { fullPage: true });
 });
