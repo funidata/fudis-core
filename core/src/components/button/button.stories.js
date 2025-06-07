5041fba9ca19610bfe42a242185ef41094c42768
marisanity <marion.karlsson@gmail.com>
2025-06-07 16:59:31 +0300

    add rest of the button variants

--- core/src/components/button/button.stories.js
+++ core/src/components/button/button.stories.js
@@ -7,12 +7,12 @@ export default {
 };
 
 export const Primary = () => `
+  <!-- With text -->
   <button class="fudis-button fudis-button__primary fudis-button__size__medium">
     Primary
   </button>
-`;
 
-export const PrimaryWithIcon = () => `
+  <!-- With text and icon -->
   <button class="fudis-button fudis-button__primary fudis-button__size__medium">
     <div class="fudis-button__icon">
         <!-- Add an icon variant of your choice and color here. -->
@@ -20,31 +20,47 @@ export const PrimaryWithIcon = () => `
     </div>
     Primary
   </button>
-`;
 
-export const Secondary = () => `
-  <button class="fudis-button fudis-button__secondary fudis-button__size__medium">
-    Secondary
+  <!-- With icon -->
+  <button class="fudis-button fudis-button__label--hidden fudis-button__primary fudis-button__size__medium">
+    <div class="fudis-button__icon">
+        <!-- Add an icon variant of your choice and color here. -->
+        <span class="fudis-button__icon fudis-icon fudis-icon__color__white fudis-icon__size--lg fudis-icon__search"></span>
+    </div>
   </button>
 `;
 
-export const SecondaryWithIcon = () => `
+export const Secondary = () => `
+  <!-- With text -->
+  <button class="fudis-button fudis-button__secondary fudis-button__size__medium">
+    Secondary
+  </button>
+
+  <!-- With text and icon -->
   <button class="fudis-button fudis-button__secondary fudis-button__size__medium">
      <div class="fudis-button__icon">
         <!-- Add an icon variant of your choice and color here. -->
         <span class="fudis-button__icon__component fudis-icon fudis-icon__color__primary fudis-icon__size--lg fudis-icon__search"></span>
-    </div>
+     </div>
     Secondary
+  </button>
+
+  <!-- With icon -->
+  <button class="fudis-button fudis-button__label--hidden fudis-button__secondary fudis-button__size__medium">
+    <div class="fudis-button__icon">
+        <!-- Add an icon variant of your choice and color here. -->
+        <span class="fudis-button__icon fudis-icon fudis-icon__color__primary fudis-icon__size--lg fudis-icon__search"></span>
+    </div>
   </button>
 `;
 
 export const Tertiary = () => `
+  <!-- With text -->
   <button class="fudis-button fudis-button__tertiary fudis-button__size__medium">
     Tertiary
   </button>
-`;
 
-export const TertiaryWithIcon = () => `
+  <!-- With text and icon -->
   <button class="fudis-button fudis-button__tertiary fudis-button__size__medium">
     <div class="fudis-button__icon">
       <!-- Add an icon variant of your choice and color here.-->
@@ -52,15 +68,23 @@ export const TertiaryWithIcon = () => `
     </div>
     Tertiary
   </button>
-`;
 
-export const Primary_Small = () => `
-  <button class="fudis-button fudis-button__primary fudis-button__size__small">
-    Primary Small
+  <!-- With icon -->
+  <button class="fudis-button fudis-button__label--hidden fudis-button__tertiary fudis-button__size__medium">
+    <div class="fudis-button__icon">
+        <!-- Add an icon variant of your choice and color here. -->
+        <span class="fudis-button__icon fudis-icon fudis-icon__color__primary fudis-icon__size--lg fudis-icon__search"></span>
+    </div>
   </button>
 `;
 
-export const Primary_Small_With_Icon = () => `
+export const Primary_Small = () => `
+  <!-- With text -->
+  <button class="fudis-button fudis-button__primary fudis-button__size__small">
+    Primary Small
+  </button>
+
+  <!-- With text and icon -->
   <button class="fudis-button fudis-button__primary fudis-button__size__small">
     <div class="fudis-button__icon">
       <!-- Add an icon of your choice and color here. -->
@@ -68,37 +92,85 @@ export const Primary_Small_With_Icon = () => `
     </div>
     Primary Small
   </button>
-`;
 
-export const Secondary_Small = () => `
-  <button class="fudis-button fudis-button__secondary fudis-button__size__small">
-    Secondary Small
+  <!-- With icon -->
+  <button class="fudis-button fudis-button__label--hidden fudis-button__primary fudis-button__size__small">
+    <div class="fudis-button__icon">
+        <!-- Add an icon variant of your choice and color here. -->
+        <span class="fudis-button__icon fudis-icon fudis-icon__color__white fudis-icon__size--lg fudis-icon__search"></span>
+    </div>
   </button>
 `;
 
-export const Secondary_Small_With_Icon = () => `
+export const Secondary_Small = () => `
+  <!-- With text -->
+  <button class="fudis-button fudis-button__secondary fudis-button__size__small">
+    Secondary Small
+  </button>
+
+  <!-- With text and icon -->
   <button class="fudis-button fudis-button__secondary fudis-button__size__small">
     <div class="fudis-button__icon">
       <!-- Add an icon of your choice and color here. -->
       <span class="fudis-button__icon__component fudis-icon fudis-icon__color__primary fudis-icon__size--lg fudis-icon__search"></span>
     </div>
     Secondary Small
+  </button>
+
+  <!-- With icon -->
+  <button class="fudis-button fudis-button__label--hidden fudis-button__secondary fudis-button__size__small">
+    <div class="fudis-button__icon">
+        <!-- Add an icon variant of your choice and color here. -->
+        <span class="fudis-button__icon fudis-icon fudis-icon__color__primary fudis-icon__size--lg fudis-icon__search"></span>
+    </div>
   </button>
 `;
 
 export const Tertiary_Small = () => `
+  <!-- With text -->
   <button class="fudis-button fudis-button__tertiary fudis-button__size__small">
     Tertiary Small
   </button>
-`;
 
-export const Tertiary_Small_With_Icon = () => `
+  <!-- With text and icon -->
   <button class="fudis-button fudis-button__tertiary fudis-button__size__small">
     <div class="fudis-button__icon">
       <!-- Add an icon of your choice and color here. -->
       <span class="fudis-button__icon__component fudis-icon fudis-icon__color__primary fudis-icon__size--lg fudis-icon__search"></span>
     </div>
     Tertiary Small
+  </button>
+
+  <!-- With icon -->
+  <button class="fudis-button fudis-button__label--hidden fudis-button__tertiary fudis-button__size__small">
+    <div class="fudis-button__icon">
+        <!-- Add an icon variant of your choice and color here. -->
+        <span class="fudis-button__icon fudis-icon fudis-icon__color__primary fudis-icon__size--lg fudis-icon__search"></span>
+    </div>
+  </button>
+`;
+
+export const Icon_Button_Primary = () => `
+  <button class="fudis-button fudis-button__label--hidden fudis-button__primary fudis-button__size__icon-only">
+    <div class="fudis-button__icon">
+        <span class="fudis-button__icon__component fudis-icon fudis-icon__color__white fudis-icon__size--lg fudis-icon__search"></span>
+    </div>
+  </button>
+`;
+
+export const Icon_Button_Tertiary = () => `
+  <button class="fudis-button fudis-button__label--hidden fudis-button__tertiary fudis-button__size__icon-only">
+    <div class="fudis-button__icon">
+        <span class="fudis-button__icon__component fudis-icon fudis-icon__color__primary fudis-icon__size--lg fudis-icon__search"></span>
+    </div>
+  </button>
+`;
+
+export const Icon_Button_Secondary = () => `
+  <button class="fudis-button fudis-button__label--hidden fudis-button__secondary fudis-button__size__icon-only">
+    <div class="fudis-button__icon">
+        <span class="fudis-button__icon__component fudis-icon fudis-icon__color__primary fudis-icon__size--lg fudis-icon__search"></span>
+    </div>
   </button>
 `;
 
