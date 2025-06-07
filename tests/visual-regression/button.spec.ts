9c839424bc015da9dbb366320bb9ccb1b7cdb2d6
marisanity <marion.karlsson@gmail.com>
2025-06-07 15:57:02 +0300

    fix preview and tests urls

--- tests/visual-regression/button.spec.ts
+++ tests/visual-regression/button.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@playwright/test";
 
 test("visual regression for Button", async ({ page }) => {
   await page.goto(
-    "/iframe.html?globals=&id=components-button-wip--test&viewMode=story",
+    "/iframe.html?globals=&id=components-button--test&viewMode=story",
   );
   await expect(page).toHaveScreenshot("buttons.png", { fullPage: true });
 });
