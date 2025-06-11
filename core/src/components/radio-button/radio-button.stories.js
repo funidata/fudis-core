5da9efb97e28a1e530f6294ca592d62ca07d87b7
RiinaKuu <riina.kuukso@funidata.fi>
2025-06-11 15:42:23 +0300

    DS-513: Add RadioButton Playwright story and test

--- core/src/components/radio-button/radio-button.stories.js
+++ core/src/components/radio-button/radio-button.stories.js
@@ -1,5 +1,9 @@
 export default {
   title: "Components/Radio Button",
+  decorators: [
+    (storyFn) =>
+      `<div style="display: flex; gap: 12px; flex-wrap: wrap; align-items: flex-start;">${storyFn()}</div>`,
+  ],
 };
 
 export const Default = () => `
@@ -58,6 +62,47 @@ export const Invalid = () => `
             <!-- When selected: render the indicator below -->
             <!-- <span class="fudis-radio-button__content__control__indicator"></span> -->
         </div>
+      </div>
+      <span class="fudis-radio-button__label">Option</span>
+    </div>
+  </label>
+`;
+
+export const PwAll = () => `
+  <label class="fudis-radio-button">
+    <input type="radio" class="fudis-radio-button__input" value="foo" />
+    <div class="fudis-radio-button__content">
+      <div class="fudis-radio-button__content-wrapper">
+        <div class="fudis-radio-button__content__control"></div>
+      </div>
+      <span class="fudis-radio-button__label">Option</span>
+    </div>
+  </label>
+  <label class="fudis-radio-button">
+    <input type="radio" class="fudis-radio-button__input" value="foo" />
+    <div class="fudis-radio-button__content">
+      <div class="fudis-radio-button__content-wrapper">
+        <div class="fudis-radio-button__content__control">
+          <span class="fudis-radio-button__content__control__indicator"></span>
+        </div>
+      </div>
+      <span class="fudis-radio-button__label">Option</span>
+    </div>
+  </label>
+  <label class="fudis-radio-button fudis-radio-button--disabled">
+    <input type="radio" class="fudis-radio-button__input fudis-radio-button__input--disabled" aria-disabled="true" disabled value="foo" />
+    <div class="fudis-radio-button__content">
+      <div class="fudis-radio-button__content-wrapper">
+        <div class="fudis-radio-button__content__control fudis-radio-button__content__control--disabled"></div>
+      </div>
+      <span class="fudis-radio-button__label">Option</span>
+    </div>
+  </label>
+  <label class="fudis-radio-button">
+    <input type="radio" class="fudis-radio-button__input" aria-invalid="true" value="foo" />
+    <div class="fudis-radio-button__content">
+      <div class="fudis-radio-button__content-wrapper">
+        <div class="fudis-radio-button__content__control fudis-radio-button__content__control--invalid"></div>
       </div>
       <span class="fudis-radio-button__label">Option</span>
     </div>
