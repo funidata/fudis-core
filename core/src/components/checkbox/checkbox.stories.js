7c577ce628ce9a1eff16c6964da4fe143c8f02cc
RiinaKuu <riina.kuukso@funidata.fi>
2025-06-11 15:21:40 +0300

    DS-513: Add Checkbox Playwright story and test

--- core/src/components/checkbox/checkbox.stories.js
+++ core/src/components/checkbox/checkbox.stories.js
@@ -84,3 +84,53 @@ export const Invalid = () => `
     </div>
   </label>
 `;
+
+export const PwAll = () => `
+  <label class="fudis-checkbox">
+    <input type="checkbox" class="fudis-checkbox__input" name="fudis-checkbox" value="foo" />
+    <div class="fudis-checkbox__content">
+       <div class="fudis-checkbox__content-wrapper">
+        <span class="fudis-checkbox__content__box"></span>
+       </div>
+       <span class="fudis-checkbox__content__label">Option</span>
+    </div>
+  </label>
+  <label class="fudis-checkbox">
+    <input type="checkbox" class="fudis-checkbox__input" name="fudis-checkbox" value="foo" />
+    <div class="fudis-checkbox__content">
+       <div class="fudis-checkbox__content-wrapper">
+        <span class="fudis-checkbox__content__box">
+            <span class="fudis-icon fudis-icon__color__gray-dark fudis-icon__lg fudis-icon__check"></span>
+        </span>
+       </div>
+       <span class="fudis-checkbox__content__label">Option</span>
+    </div>
+  </label>
+  <label class="fudis-checkbox">
+    <input type="checkbox" class="fudis-checkbox__input" name="fudis-checkbox" value="foo" />
+    <div class="fudis-checkbox__content">
+       <div class="fudis-checkbox__content-wrapper">
+        <span class="fudis-checkbox__content__box fudis-checkbox__content__box--focused"></span>
+       </div>
+       <span class="fudis-checkbox__content__label">Option</span>
+    </div>
+  </label>
+  <label class="fudis-checkbox fudis-checkbox--disabled">
+    <input type="checkbox" class="fudis-checkbox__input" name="fudis-checkbox" value="foo" aria-disabled="true" disabled />
+    <div class="fudis-checkbox__content">
+       <div class="fudis-checkbox__content-wrapper">
+        <span class="fudis-checkbox__content__box fudis-checkbox__content__box--disabled"></span>
+       </div>
+       <span class="fudis-checkbox__content__label">Option</span>
+    </div>
+  </label>
+  <label class="fudis-checkbox">
+    <input type="checkbox" class="fudis-checkbox__input" name="fudis-checkbox" value="foo" aria-invalid="true" />
+    <div class="fudis-checkbox__content">
+       <div class="fudis-checkbox__content-wrapper">
+        <span class="fudis-checkbox__content__box fudis-checkbox__content__box--invalid"></span>
+       </div>
+       <span class="fudis-checkbox__content__label">Option</span>
+    </div>
+  </label>
+`
