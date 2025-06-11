9ed25f3b004ca201d834197bcbc6b015fe31f79e
RiinaKuu <riina.kuukso@funidata.fi>
2025-06-11 15:16:04 +0300

    DS-513: Add Icon Button Playwright story and test

--- core/src/components/button/icon-button.stories.js
+++ core/src/components/button/icon-button.stories.js
@@ -91,4 +91,83 @@ export const Disabled = () => `
         <span class="fudis-button__icon__component fudis-icon fudis-icon__color__white fudis-icon__lg fudis-icon__search"></span>
     </div>
   </button>
+
+  <!-- Size: medium -->
+  <button aria-disabled="true" class="fudis-button fudis-button__label--hidden fudis-button__primary fudis-button__size__medium">
+    <div class="fudis-button__icon">
+        <!-- Add an icon variant of your choice and color here. -->
+        <span class="fudis-button__icon__component fudis-icon fudis-icon__color__white fudis-icon__lg fudis-icon__search"></span>
+    </div>
+  </button>
+
+  <!-- Size: Small -->
+  <button aria-disabled="true" class="fudis-button fudis-button__label--hidden fudis-button__primary fudis-button__size__small">
+    <div class="fudis-button__icon">
+        <!-- Add an icon variant of your choice and color here. -->
+        <span class="fudis-button__icon__component fudis-icon fudis-icon__color__white fudis-icon__lg fudis-icon__search"></span>
+    </div>
+  </button>
 `;
+
+export const PwAll = () => `
+  <button class="fudis-button fudis-button__label--hidden fudis-button__primary fudis-button__size__icon-only">
+    <div class="fudis-button__icon">
+        <span class="fudis-button__icon__component fudis-icon fudis-icon__color__white fudis-icon__lg fudis-icon__search"></span>
+    </div>
+  </button>
+  <button class="fudis-button fudis-button__label--hidden fudis-button__primary fudis-button__size__medium">
+    <div class="fudis-button__icon">
+        <span class="fudis-button__icon fudis-icon fudis-icon__color__white fudis-icon__lg fudis-icon__search"></span>
+    </div>
+  </button>
+  <button class="fudis-button fudis-button__label--hidden fudis-button__primary fudis-button__size__small">
+    <div class="fudis-button__icon">
+        <span class="fudis-button__icon fudis-icon fudis-icon__color__white fudis-icon__lg fudis-icon__search"></span>
+    </div>
+  </button>
+  <button class="fudis-button fudis-button__label--hidden fudis-button__secondary fudis-button__size__icon-only">
+    <div class="fudis-button__icon">
+        <span class="fudis-button__icon__component fudis-icon fudis-icon__color__primary fudis-icon__lg fudis-icon__search"></span>
+    </div>
+  </button>
+  <button class="fudis-button fudis-button__label--hidden fudis-button__secondary fudis-button__size__medium">
+    <div class="fudis-button__icon">
+        <span class="fudis-button__icon fudis-icon fudis-icon__color__primary fudis-icon__lg fudis-icon__search"></span>
+    </div>
+  </button>
+  <button class="fudis-button fudis-button__label--hidden fudis-button__secondary fudis-button__size__small">
+    <div class="fudis-button__icon">
+        <span class="fudis-button__icon fudis-icon fudis-icon__color__primary fudis-icon__lg fudis-icon__search"></span>
+    </div>
+  </button>
+    <button class="fudis-button fudis-button__label--hidden fudis-button__tertiary fudis-button__size__icon-only">
+    <div class="fudis-button__icon">
+        <span class="fudis-button__icon__component fudis-icon fudis-icon__color__primary fudis-icon__lg fudis-icon__search"></span>
+    </div>
+  </button>
+  <button class="fudis-button fudis-button__label--hidden fudis-button__tertiary fudis-button__size__medium">
+    <div class="fudis-button__icon">
+        <span class="fudis-button__icon fudis-icon fudis-icon__color__primary fudis-icon__lg fudis-icon__search"></span>
+    </div>
+  </button>
+  <button class="fudis-button fudis-button__label--hidden fudis-button__tertiary fudis-button__size__small">
+    <div class="fudis-button__icon">
+        <span class="fudis-button__icon fudis-icon fudis-icon__color__primary fudis-icon__lg fudis-icon__search"></span>
+    </div>
+  </button>
+  <button aria-disabled="true" class="fudis-button fudis-button__label--hidden fudis-button__primary fudis-button__size__icon-only">
+    <div class="fudis-button__icon">
+        <span class="fudis-button__icon__component fudis-icon fudis-icon__color__white fudis-icon__lg fudis-icon__search"></span>
+    </div>
+  </button>
+  <button aria-disabled="true" class="fudis-button fudis-button__label--hidden fudis-button__primary fudis-button__size__medium">
+    <div class="fudis-button__icon">
+        <span class="fudis-button__icon__component fudis-icon fudis-icon__color__white fudis-icon__lg fudis-icon__search"></span>
+    </div>
+  </button>
+  <button aria-disabled="true" class="fudis-button fudis-button__label--hidden fudis-button__primary fudis-button__size__small">
+    <div class="fudis-button__icon">
+        <span class="fudis-button__icon__component fudis-icon fudis-icon__color__white fudis-icon__lg fudis-icon__search"></span>
+    </div>
+  </button>
+`
