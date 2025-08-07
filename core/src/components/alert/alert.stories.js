d251dbea3eba4838af259f0d4d2178735754655f
marjutkinnari <marjut.kinnari@funidata.fi>
2025-08-07 09:52:26 +0300

    DS-527: Fixed VR test for all

--- core/src/components/alert/alert.stories.js
+++ core/src/components/alert/alert.stories.js
@@ -1,51 +1,75 @@
 export default {
   title: "Components/Alert",
   argTypes: {
-    label: {
-      name: "Label",
+    message: {
+      name: "Alert",
       control: { type: "text" },
     },
     variant: {
-      options: ['warning', 'danger', 'success', 'info'],
-      control: { type: 'radio' },
-    }
-  }
+      options: ["warning", "danger", "success", "info"],
+      control: { type: "radio" },
+    },
+  },
 };
 
-const Template = ({
-  label,
-  variant,
-}) => {
-    const alertDiv = document.createElement('div');
-    alertDiv.className = `fudis-alert fudis-alert__${variant}`;
+const Template = ({ message, variant }) => {
+  const alertDiv = document.createElement("div");
+  alertDiv.className = `fudis-alert fudis-alert__${variant}`;
 
-    const alertText = document.createElement('p');
-    alertText.className = 'fudis-body-text fudis-body-text__center fudis-body-text__lg-regular fudis-alert__text';
-    alertText.textContent = label;
+  const alertText = document.createElement("p");
+  alertText.className =
+    "fudis-body-text fudis-body-text__center fudis-body-text__lg-regular fudis-alert__text";
+  alertText.textContent = message;
 
-    const alertCloseButton = document.createElement('button');
-    alertCloseButton.className = `fudis-alert__close fudis-alert__close__${variant}`;
-    alertCloseButton.setAttribute('aria-label', `${variant.charAt(0).toUpperCase() + variant.slice(1)}, close`);
+  const alertCloseButton = document.createElement("button");
+  alertCloseButton.className = `fudis-alert__close fudis-alert__close__${variant}`;
+  alertCloseButton.setAttribute(
+    "aria-label",
+    `${variant.charAt(0).toUpperCase() + variant.slice(1)}, close`,
+  );
 
-    const alertIcon = document.createElement('span');
+  const alertIcon = document.createElement("span");
 
-    const iconColor = variant === 'warning' 
-  ? 'fudis-icon__color__gray-dark' 
-  : 'fudis-icon__color__white';
+  const iconColor =
+    variant === "warning"
+      ? "fudis-icon__color__gray-dark"
+      : "fudis-icon__color__white";
 
-    alertIcon.className = `fudis-icon ${iconColor} fudis-icon__lg fudis-icon__close`;;
+  alertIcon.className = `fudis-icon ${iconColor} fudis-icon__lg fudis-icon__close`;
 
-    alertCloseButton.appendChild(alertIcon);
-    alertDiv.appendChild(alertText);
-    alertDiv.appendChild(alertCloseButton);
+  alertCloseButton.appendChild(alertIcon);
+  alertDiv.appendChild(alertText);
+  alertDiv.appendChild(alertCloseButton);
 
-    return alertDiv;
-}
+  return alertDiv;
+};
 
 let defaultValues = {
-  label: "Alert",
-  variant: 'danger',
+  message: "Alert",
+  variant: "danger",
 };
 
 export const Alert = Template.bind({});
 Alert.args = defaultValues;
+
+export const PwAll = () => {
+  const configurations = [
+    { message: "Danger", variant: "danger" },
+    { message: "Info", variant: "info" },
+    { message: "Success", variant: "success" },
+    { message: "Warning", variant: "warning" },
+  ];
+
+  const alertGroupWrapper = document.createElement("div");
+
+  configurations.forEach((config) => {
+    const element = Template({
+      ...defaultValues,
+      ...config,
+    });
+
+    alertGroupWrapper.appendChild(element);
+  });
+
+  return alertGroupWrapper;
+};
