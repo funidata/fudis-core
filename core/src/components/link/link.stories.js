9eb2776f8c707f61f846256af3864296ce8c8fae
marjutkinnari <marjut.kinnari@funidata.fi>
2025-08-08 09:20:41 +0300

    DS-527: Link and Loading VR update

--- core/src/components/link/link.stories.js
+++ core/src/components/link/link.stories.js
@@ -1,26 +1,50 @@
 export default {
   title: "Components/Link (WIP)",
+  argTypes: {
+    size: {
+      options: ["md", "lg"],
+      control: { type: "radio" },
+    },
+    title: {
+      name: "title",
+      control: { type: "text" },
+    },
+  },
 };
 
 // TODO: Add external link stories after icons are available
 
-export const Medium = () => `
-  <a class="fudis-link fudis-link__size__md">
-    I am a medium sized link
-  </a>
-`;
+const Template = ({ size, title }) => {
+  const link = document.createElement("a");
+  link.setAttribute("href", "");
+  link.className = `fudis-link fudis-link__size__${size}`;
+  link.textContent = title;
 
-export const Large = () => `
-  <a class="fudis-link fudis-link__size__lg">
-    I am a large sized link
-  </a>
-`;
+  return link;
+};
 
-export const PwAll = () => `
-  <a href="" class="fudis-link fudis-link__size__md">
-    I am a medium sized link
-  </a>
-  <a href="" class="fudis-link fudis-link__size__lg">
-    I am a large sized link
-  </a>
-`;
+let defaultValues = {
+  size: "md",
+  title: "I am a link",
+};
+
+export const Link = Template.bind({});
+Link.args = defaultValues;
+
+export const PwAll = () => {
+  const configurations = [
+    { size: "md", title: "I am a medium sized link" },
+    { size: "lg", title: "I am a large sized link" },
+  ];
+
+  return configurations
+    .map((config) => {
+      const element = Template({
+        ...defaultValues,
+        ...config,
+      });
+
+      return element.outerHTML;
+    })
+    .join(" ");
+};
