d844bf7b7e07bbc6886a7415d8050b1db3fb451e
marjutkinnari <marjut.kinnari@funidata.fi>
2025-08-08 12:58:30 +0300

    DS-527: Updated new test url paths

--- tests/visual-regression/error-summary.spec.ts
+++ tests/visual-regression/error-summary.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@playwright/test";
 
 test("visual regression for ErrorSummary", async ({ page }) => {
   await page.goto(
-    "/iframe.html?globals=&id=components-error-summary--default&viewMode=story",
+    "/iframe.html?args=&globals=&id=components-error-summary--example&viewMode=story",
   );
   await expect(page).toHaveScreenshot("error-summary.png", { fullPage: true });
 });
