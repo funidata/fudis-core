03d75a9f2abcbcfbdd63a28e76f382d1eaff7867
marjutkinnari <marjut.kinnari@funidata.fi>
2025-08-18 13:30:50 +0300

    DS-532: VR image update

--- tests/visual-regression/icon.spec.ts
+++ tests/visual-regression/icon.spec.ts
@@ -2,21 +2,21 @@ import { expect, test } from "@playwright/test";
 
 test("visual regression for Icon", async ({ page }) => {
   await page.goto(
-    "/iframe.html?globals=&args=&id=components-icon-wip--all-icons&viewMode=story",
+    "/iframe.html?globals=&args=&id=components-icon--all-icons&viewMode=story",
   );
   await expect(page).toHaveScreenshot("icons.png", { fullPage: true });
 });
 
 test("visual regression for Icon colors", async ({ page }) => {
   await page.goto(
-    "/iframe.html?globals=&args=&id=components-icon-wip--pw-icon-colors&viewMode=story",
+    "/iframe.html?globals=&args=&id=components-icon--pw-icon-colors&viewMode=story",
   );
   await expect(page).toHaveScreenshot("icon-color.png", { fullPage: true });
 });
 
 test("visual regression for Icon rotate", async ({ page }) => {
   await page.goto(
-    "/iframe.html?globals=&args=&id=components-icon-wip--pw-icon-rotate&viewMode=story",
+    "/iframe.html?globals=&args=&id=components-icon--pw-icon-rotate&viewMode=story",
   );
   await expect(page).toHaveScreenshot("icon-rotate.png", { fullPage: true });
 });
