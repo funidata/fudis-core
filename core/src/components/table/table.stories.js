ad299b5be083b2ed079f147c71af031d77f0e05a
RiinaKuu <riina.kuukso@funidata.fi>
2025-10-09 12:42:49 +0300

    DS-567: Delete variant from args and template, delete unnecessary Playwright templates

--- core/src/components/table/table.stories.js
+++ core/src/components/table/table.stories.js
@@ -1,10 +1,6 @@
 export default {
   title: "Components/Table",
   argTypes: {
-    variant: {
-      options: ["default", "mobile"],
-      control: { type: "radio" },
-    },
     alignHeader: {
       options: ["left", "center", "right"],
       control: { type: "radio" },
@@ -22,29 +18,29 @@ export default {
 
 const html = String.raw;
 
-const Template = ({ variant, alignHeader, alignCell, verticalAlignCell }) => {
+const Template = ({ alignHeader, alignCell, verticalAlignCell }) => {
   return html`
-    <table class="fudis-table fudis-table__${variant}">
-      <caption class="fudis-table__${variant}__caption">
+    <table class="fudis-table fudis-table">
+      <caption class="fudis-table__caption">
         Course information
       </caption>
       <thead>
         <tr>
           <th
             scope="col"
-            class="fudis-table__${variant}__header fudis-table__${variant}__header__align__${alignHeader}"
+            class="fudis-table__header fudis-table__header__align__${alignHeader}"
           >
             Code
           </th>
           <th
             scope="col"
-            class="fudis-table__${variant}__header fudis-table__${variant}__header__align__${alignHeader}"
+            class="fudis-table__header fudis-table__header__align__${alignHeader}"
           >
             Title
           </th>
           <th
             scope="col"
-            class="fudis-table__${variant}__header fudis-table__${variant}__header__align__${alignHeader}"
+            class="fudis-table__header fudis-table__header__align__${alignHeader}"
           >
             Curriculum periods
           </th>
@@ -53,52 +49,52 @@ const Template = ({ variant, alignHeader, alignCell, verticalAlignCell }) => {
       <tbody>
         <tr>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             AUT.116
           </td>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             Information Technology Applications in Automation
           </td>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             2024-2025
           </td>
         </tr>
         <tr>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             URLA-10
           </td>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             Urban Laboratory II (studio)
           </td>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             2025-2026
           </td>
         </tr>
         <tr>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             DES-12.335
           </td>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             Architectural Design and Integrated Sustainable Systems for Urban
             Environments and Resilient Infrastructures in the 21st Century
           </td>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             2025-2026
           </td>
@@ -109,21 +105,20 @@ const Template = ({ variant, alignHeader, alignCell, verticalAlignCell }) => {
 };
 
 const SelectableTemplate = ({
-  variant,
   alignHeader,
   alignCell,
   verticalAlignCell,
 }) => {
   return html`
-    <table class="fudis-table fudis-table__${variant}">
-      <caption id="table-caption-id" class="fudis-table__${variant}__caption">
+    <table class="fudis-table fudis-table">
+      <caption id="table-caption-id" class="fudis-table__caption">
         Course selection
       </caption>
       <thead>
         <tr>
           <th
             scope="col"
-            class="fudis-table__${variant}__header fudis-table__${variant}__header__align__${alignHeader}"
+            class="fudis-table__header fudis-table__header__align__${alignHeader}"
           >
             <label class="fudis-checkbox">
               <input
@@ -141,19 +136,19 @@ const SelectableTemplate = ({
           </th>
           <th
             scope="col"
-            class="fudis-table__${variant}__header fudis-table__${variant}__header__align__${alignHeader}"
+            class="fudis-table__header fudis-table__header__align__${alignHeader}"
           >
             Code
           </th>
           <th
             scope="col"
-            class="fudis-table__${variant}__header fudis-table__${variant}__header__align__${alignHeader}"
+            class="fudis-table__header fudis-table__header__align__${alignHeader}"
           >
             Title
           </th>
           <th
             scope="col"
-            class="fudis-table__${variant}__header fudis-table__${variant}__header__align__${alignHeader}"
+            class="fudis-table__header fudis-table__header__align__${alignHeader}"
           >
             Curriculum periods
           </th>
@@ -162,7 +157,7 @@ const SelectableTemplate = ({
       <tbody>
         <tr>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__checkbox fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__checkbox fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             <label class="fudis-checkbox">
               <input
@@ -179,25 +174,25 @@ const SelectableTemplate = ({
             </label>
           </td>
           <td
-            id="course-aut"
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             AUT.116
           </td>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            id="course-aut"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             Information Technology Applications in Automation
           </td>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             2024-2025
           </td>
         </tr>
         <tr>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__checkbox fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__checkbox fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             <label class="fudis-checkbox">
               <input
@@ -214,25 +209,25 @@ const SelectableTemplate = ({
             </label>
           </td>
           <td
-            id="course-urla"
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             URLA-10
           </td>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            id="course-urla"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             Urban Laboratory II (studio)
           </td>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             2025-2026
           </td>
         </tr>
         <tr>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__checkbox fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__checkbox fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             <label class="fudis-checkbox">
               <input
@@ -253,19 +248,19 @@ const SelectableTemplate = ({
             </label>
           </td>
           <td
-            id="course-des"
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             DES-12.335
           </td>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            id="course-des"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             Architectural Design and Integrated Sustainable Systems for Urban
             Environments and Resilient Infrastructures in the 21st Century
           </td>
           <td
-            class="fudis-table__${variant}__cell fudis-table__${variant}__cell__align__${alignCell} fudis-table__${variant}__cell__vertical-align__${verticalAlignCell}"
+            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
           >
             2025-2026
           </td>
@@ -276,7 +271,6 @@ const SelectableTemplate = ({
 };
 
 let defaultValues = {
-  variant: "default",
   alignHeader: "left",
   alignCell: "left",
   verticalAlignCell: "top",
@@ -292,15 +286,6 @@ export const PwAll = () => {
   return html`
     <div style="margin-bottom: 2rem">
       ${Template({
-        variant: "default",
-        alignHeader: "left",
-        alignCell: "left",
-        verticalAlignCell: "top",
-      })}
-    </div>
-    <div style="margin-bottom: 2rem">
-      ${Template({
-        variant: "mobile",
         alignHeader: "left",
         alignCell: "left",
         verticalAlignCell: "top",
@@ -308,15 +293,6 @@ export const PwAll = () => {
     </div>
     <div style="margin-bottom: 2rem">
       ${SelectableTemplate({
-        variant: "default",
-        alignHeader: "left",
-        alignCell: "left",
-        verticalAlignCell: "top",
-      })}
-    </div>
-    <div style="margin-bottom: 2rem">
-      ${SelectableTemplate({
-        variant: "mobile",
         alignHeader: "left",
         alignCell: "left",
         verticalAlignCell: "top",
