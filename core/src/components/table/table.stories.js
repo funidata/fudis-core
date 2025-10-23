008f1f692f300efa496e9595787d991eadb85582
RiinaKuu <riina.kuukso@funidata.fi>
2025-10-23 15:36:17 +0300

    DS-588: Add sorter button to the first table header, add controls and necessary CSS class logic. Put example data into array in order to demonstrate sorting alphabetically.

--- core/src/components/table/table.stories.js
+++ core/src/components/table/table.stories.js
@@ -13,12 +13,58 @@ export default {
       options: ["top", "center", "bottom"],
       control: { type: "radio" },
     },
+    ariaSort: {
+      options: ["ascending", "descending", "none"],
+      control: { type: "radio" },
+    },
   },
 };
 
 const html = String.raw;
 
-const Template = ({ alignHeader, alignCell, verticalAlignCell }) => {
+const Template = ({ alignHeader, alignCell, verticalAlignCell, ariaSort }) => {
+  // Aria-sort attribute should not be in the DOM if aria-sort is none
+  const ariaSortAttribute =
+    ariaSort && ariaSort !== "none" ? `aria-sort="${ariaSort}"` : "";
+
+  // Add icon rotate class based on ariaSort control value
+  const rotateClass =
+    ariaSort === "descending" ? "fudis-icon__rotate__flip-180" : "";
+
+  // Add active header class based on ariaSort control value, only currently sorted header should have this class
+  const activeSortableHeader =
+    (ariaSort === "descending") | (ariaSort === "ascending")
+      ? "fudis-table__header--active"
+      : "";
+
+  const courseData = [
+    {
+      code: "AUT.116",
+      title: "Information Technology Applications in Automation",
+      period: "2024-2025",
+    },
+    {
+      code: "URLA-10",
+      title: "Urban Laboratory II (studio)",
+      period: "2025-2026",
+    },
+    {
+      code: "DES-12.335",
+      title:
+        "Architectural Design and Integrated Sustainable Systems for Urban Environments and Resilient Infrastructures in the 21st Century",
+      period: "2025-2026",
+    },
+  ];
+
+  const sortedCourseData = [...courseData];
+
+  // Sort example data alphabetically based on ariaSort control value
+  if (ariaSort === "ascending") {
+    sortedCourseData.sort((a, b) => a.code.localeCompare(b.code));
+  } else if (ariaSort === "descending") {
+    sortedCourseData.sort((a, b) => b.code.localeCompare(a.code));
+  }
+
   return html`
     <table class="fudis-table">
       <caption class="fudis-table__caption">
@@ -28,9 +74,20 @@ const Template = ({ alignHeader, alignCell, verticalAlignCell }) => {
         <tr>
           <th
             scope="col"
-            class="fudis-table__header fudis-table__header__align__${alignHeader}"
+            class="fudis-table__header fudis-table__header__align__${alignHeader} ${activeSortableHeader}"
+            ${ariaSortAttribute}
           >
             Code
+            <button
+              class="fudis-button fudis-button__tertiary fudis-button__size__extra-small"
+              aria-label="Sort column data alphabetically"
+            >
+              <div class="fudis-button__icon">
+                <span
+                  class="fudis-button__icon__component fudis-icon fudis-icon__color__primary fudis-icon__lg fudis-icon__sorter ${rotateClass}"
+                ></span>
+              </div>
+            </button>
           </th>
           <th
             scope="col"
@@ -47,58 +104,23 @@ const Template = ({ alignHeader, alignCell, verticalAlignCell }) => {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td
-            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
-          >
-            AUT.116
-          </td>
-          <td
-            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
-          >
-            Information Technology Applications in Automation
-          </td>
-          <td
-            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
-          >
-            2024-2025
-          </td>
-        </tr>
-        <tr>
-          <td
-            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
-          >
-            URLA-10
-          </td>
-          <td
-            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
-          >
-            Urban Laboratory II (studio)
-          </td>
-          <td
-            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
-          >
-            2025-2026
-          </td>
-        </tr>
-        <tr>
-          <td
-            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
-          >
-            DES-12.335
-          </td>
-          <td
-            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
-          >
-            Architectural Design and Integrated Sustainable Systems for Urban
-            Environments and Resilient Infrastructures in the 21st Century
-          </td>
-          <td
-            class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}"
-          >
-            2025-2026
-          </td>
-        </tr>
+        ${sortedCourseData
+          .map(
+            (row) => `
+          <tr>
+            <td class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}">
+              ${row.code}
+            </td>
+            <td class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}">
+              ${row.title}
+            </td>
+            <td class="fudis-table__cell fudis-table__cell__align__${alignCell} fudis-table__cell__vertical-align__${verticalAlignCell}">
+              ${row.period}
+            </td>
+          </tr>
+          `,
+          )
+          .join("")}
       </tbody>
     </table>
   `;
@@ -260,6 +282,7 @@ let defaultValues = {
   alignHeader: "left",
   alignCell: "left",
   verticalAlignCell: "top",
+  ariaSort: "none",
 };
 
 export const Example = Template.bind({});
@@ -275,6 +298,7 @@ export const PwAll = () => {
         alignHeader: "left",
         alignCell: "left",
         verticalAlignCell: "top",
+        ariaSort: "none",
       })}
     </div>
   `;
